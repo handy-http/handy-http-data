ca8eeea49cb205caf5ded0af2354182fdda7e817
andrewlalis <me@andrewlalis.com>
2026-08-29 17:30:27 -0400

    Added pagination.

--- source/handy_http_data/package.d
+++ source/handy_http_data/package.d
@@ -4,3 +4,4 @@ public import handy_http_data.json;
 public import handy_http_data.multipart;
 public import handy_http_data.xml;
 public import handy_http_data.csv;
+public import handy_http_data.pagination;
